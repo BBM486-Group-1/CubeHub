88dca56062f9275dca6163beceece979d70c111a
M. Şamil Ateşoğlu <m.samilatesoglu@gmail.com>
2021-04-14 17:19:20 +0300

    Refactor

--- Assets/Scripts/Behaviour/GameController.cs
+++ Assets/Scripts/Behaviour/GameController.cs
@@ -1,36 +1,38 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
+namespace Behaviour
 {
-    [SerializeField] private int numberOfCubes;
+    public class GameController : MonoBehaviour
+    {
+        [SerializeField] private int numberOfCubes;
     
-    private ArrayList cubes; 
+        private ArrayList _cubes; 
 
-    [SerializeField] private Cursor _cursor;
+        [SerializeField] private Cursor _cursor;
 
-    [SerializeField] private InputHandler inputHandler;
+        [SerializeField] private InputHandler inputHandler;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        cubes = new ArrayList();
+        // Start is called before the first frame update
+        void Start()
+        {
+            _cubes = new ArrayList();
 
-        for (int i = 0; i < numberOfCubes; i++ ){
-            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            int x = Random.Range(-20, 20);
-            int y = Random.Range(-5,   5);
-            int z = Random.Range(-20, 20);
-            cube.transform.position = new Vector3(x, y, z);
-            cubes.Add(cube);
+            for (int i = 0; i < numberOfCubes; i++ ){
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                int x = Random.Range(-20, 20);
+                int y = Random.Range(-5,   5);
+                int z = Random.Range(-20, 20);
+                cube.transform.position = new Vector3(x, y, z);
+                _cubes.Add(cube);
+            }
+        
         }
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
+        }
     }
 }
--- Assets/Scripts/Behaviour/InputHandler.cs
+++ Assets/Scripts/Behaviour/InputHandler.cs
@@ -1,54 +1,52 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using Behaviour;
-using Command;
+﻿using System.Collections.Generic;
 using Domain.Command;
 using UnityEngine;
-using UnityEngine.Serialization;
 using MoveRightCommand = Domain.Command.MoveRightCommand;
 
 
-public class InputHandler : MonoBehaviour
-{ 
-    private Dictionary<string, ICommand> _commands;
+namespace Behaviour
+{
+    public class InputHandler : MonoBehaviour
+    { 
+        private Dictionary<string, ICommand> _commands;
 
-    [SerializeField] private CursorController cursorController;
+        [SerializeField] private CursorController cursorController;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        _commands = new Dictionary<string, ICommand>();
-        
-        RegisterCommand("a", new MoveLeftCommand(cursorController.GetCursor()));
-        RegisterCommand("d", new MoveRightCommand(cursorController.GetCursor())); 
-        RegisterCommand(" ", new ToggleSelectCommand(cursorController.GetCursor()));
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.anyKeyDown)
+        // Start is called before the first frame update
+        void Start()
         {
-            if (_commands.ContainsKey(Input.inputString))
+            _commands = new Dictionary<string, ICommand>();
+        
+            RegisterCommand("a", new MoveLeftCommand(cursorController.GetCursor()));
+            RegisterCommand("d", new MoveRightCommand(cursorController.GetCursor())); 
+            RegisterCommand(" ", new ToggleSelectCommand(cursorController.GetCursor()));
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.anyKeyDown)
             {
-                KeyPressed(Input.inputString);
+                if (_commands.ContainsKey(Input.inputString))
+                {
+                    KeyPressed(Input.inputString);
+                }
             }
         }
-    }
 
-    public void RegisterCommand(string key, ICommand command){ 
-        _commands[key] = command;
-    }
+        public void RegisterCommand(string key, ICommand command){ 
+            _commands[key] = command;
+        }
 
-    public void KeyPressed(string key) {
-        _commands[key].Execute();
+        public void KeyPressed(string key) {
+            _commands[key].Execute();
         
-        // this.undoButton = buttons[buttonNo];
-    }
+            // this.undoButton = buttons[buttonNo];
+        }
 
-    public void UndoKeyPressed() {
+        public void UndoKeyPressed() {
          
-    }
+        }
 
+    }
 }
