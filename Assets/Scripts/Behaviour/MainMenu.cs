44c10c44008f385f2cfb355237bbc5ba2bab9b68
Yusuf Keten <ketenyusuf@gmail.com>
2021-04-16 00:24:36 +0300

    Input handler - GUI connection

--- Assets/Scripts/Behaviour/MainMenu.cs
+++ Assets/Scripts/Behaviour/MainMenu.cs
@@ -5,31 +5,21 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System;
-
-
-public interface Command
-{
-    void execute();
-};
-
-public class UpCommand : Command
-{
-    public void execute()
-    {
-        Debug.Log("Up Command is called");
-    }
-}
-
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
     bool isInputChangeActive = false;
+    public static KeyCode[] inputs;
+
+
     public Button activeButton;
     public EventSystem eventSystem;
+
     public void PlayGame()
     {
         SceneManager.LoadScene("MainScene");
-    }   
+    }
 
     public void QuitGame()
     {
@@ -56,15 +46,46 @@ public class MainMenu : MonoBehaviour
                 activeButton.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = kcode.ToString();
             }
         }
-        
+        loadInputs();
     }
 
+    void Start()
+    {
+        
+        loadInputs();
+    }
     void Update()
     {
         if (this.isInputChangeActive)
         {
             detectPressedKeyOrButton();
         }
+    }
+
+    KeyCode stringToKeyCode(string key)
+    {
+        KeyCode thisKeyCode = (KeyCode)System.Enum.Parse(typeof(KeyCode), key);
+        return thisKeyCode;
+    }
+
+
+    void loadInputs()
+    {
+        
+        Button[] buttons = GameObject.Find("Buttons").GetComponentsInChildren<Button>(true);
+        int buttonsNumber = buttons.Length;
+        inputs = new KeyCode[buttonsNumber];
+
+        for(int i = 0; i < buttonsNumber; i++)
+        {
+            string currentButtonText = buttons[i].GetComponentInChildren<TMP_Text>().text;
+            KeyCode currentKeyCode = stringToKeyCode(currentButtonText);
+            inputs[i] = currentKeyCode;
+        }
+
+
+        if(GameObject.Find("MainMenu") && GameObject.Find("MainMenu").activeSelf)
+            GameObject.Find("OptionsMenu").SetActive(false);
     }
 }
 
